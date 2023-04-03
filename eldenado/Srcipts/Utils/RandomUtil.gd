80c649832d2aa92a4f43e0ec8e8e807ce2de5ec0
Eldan <76548040@qq.com>
2023-04-03 17:26:05 +0800

    random update

--- eldenado/Srcipts/Utils/RandomUtil.gd
+++ eldenado/Srcipts/Utils/RandomUtil.gd
@@ -2,18 +2,16 @@ extends Resource
 class_name RandomUtil
 
 class AttackPower:
-	const _RATE_V = 0
-	const _HIT_MARK = 1
 	const _RATE_ATK:float = 2.5
-	const _RATE_CRIT_ATK:float = 10.0
+	const _RATE_CRIT_ATK:float = 25.0
 	const _NO_DAMAGE_CRIT_MOD = 0.85
 
 	const _MAX_ATK_INC = 0.7
-	const _MAX_ATK_DMG_INC = 5
+	const _MAX_ATK_DMG_INC = 10
 
 	const _DEFAULT_CRIT = 1.1
-	const _MAX_CRIT_ATK_INC = 0.5
-	const _MAX_CRIT_ATK_DMG_INC = 3
+	const _MAX_CRIT_ATK_INC = 0
+	const _MAX_CRIT_ATK_DMG_INC = 0
 
 	var r := RandomNumberGenerator.new()
 	var v_min:int
@@ -37,12 +35,10 @@ class AttackPower:
 		return max(0, value)
 
 	func attack() -> bool:
-		var _v = r.randf_range(_RATE_ATK - cap_value(_ATK_INC), _RATE_V)
-		return is_hit(_v)
+		return is_hit(_RATE_ATK - cap_value(_ATK_INC))
 
 	func crit() -> bool:
-		var _v = r.randf_range(_RATE_CRIT_ATK - cap_value(_CRIT_ATK_INC), _RATE_V)
-		return is_hit(_v)
+		return is_hit(_RATE_CRIT_ATK - cap_value(_CRIT_ATK_INC))
 
 	func atk_damage():
 		if !attack():
@@ -67,7 +63,9 @@ class AttackPower:
 			v_min = round((v_min - v_max) + 0.5)
 
 	func is_hit(rate:float) -> bool:
-		return rate < _HIT_MARK
+		var n = r.randf()
+		var _c = 1.0 / (rate * 1.0)
+		return n < _c
 
 	func get_damage() -> int:
 		fix_reversed_damage()
@@ -79,7 +77,7 @@ class AttackPower:
 		crit_damage()
 		return damage
 
-static func get_attck_power(min_:int, max_:int):
+static func get_attack_power(min_:int, max_:int):
 	var p = AttackPower.new(min_, max_)
 	return p.calc()
 
@@ -87,4 +85,14 @@ static func get_attack_power_player(player:PlayerObj):
 	var _min = 0
 	var _max = 0
 
-	return get_attck_power(_min,_max)
+	return get_attack_power(_min,_max)
+
+static func get_random(chance:int):
+	var _r = RandomNumberGenerator.new()
+	_r.randomize()
+	var n = _r.randf()
+	var _c = 1.0 / (chance * 1.0)
+	var res = -1
+	if n < _c:
+		 res = n
+	return res
--- eldenado/Test/Srcipts/Utils/RandomUtilTest.gd
+++ eldenado/Test/Srcipts/Utils/RandomUtilTest.gd
@@ -7,10 +7,17 @@ extends GdUnitTestSuite
 # TestSuite generated from
 const __source = 'res://Srcipts/Utils/RandomUtil.gd'
 
-func test_get_attck_power() -> void:
+func test_get_attack_power() -> void:
 	var dps = 0
 	for i in range(0,10):
-		var a = RandomUtil.get_attck_power(1,15)
+		var a = RandomUtil.get_attack_power(1,15)
 		dps = dps + a
 		print(a)
 	print("dps->",dps)
+
+#func test_get_random() -> void:
+#	for i in range(3):
+#		var a = RandomUtil.get_random(3)
+#		if a > 0:
+#			print("{0}:{1}".format([i,a]))
+
