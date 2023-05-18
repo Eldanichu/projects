1f332681c011c03bc0b36ba086b5cce852c24d86
Eldan <76548040@qq.com>
2023-05-18 23:32:53 +0800

    implementing key bindings

--- eldenado/Store/Modules/Settings.gd
+++ eldenado/Store/Modules/Settings.gd
@@ -5,3 +5,17 @@ var fullscreen = false
 var v_sync = false
 
 var use_short_number = false
+
+var key_bindings:Dictionary = {
+	"Attack":{"key_code":-1,"key":null},
+	"Skill":{"key_code":-1,"key":null},
+	"sep":1,
+	"Skill 1":{"key_code":-1,"key":null},
+	"Skill 2":{"key_code":-1,"key":null},
+	"Skill 3":{"key_code":-1,"key":null},
+	"Skill 4":{"key_code":-1,"key":null},
+	"Skill 5":{"key_code":-1,"key":null},
+	"Skill 6":{"key_code":-1,"key":null},
+	"Skill 7":{"key_code":-1,"key":null},
+	"Skill 8":{"key_code":-1,"key":null}
+}
--- eldenado/UI/Panels/GameSettings/game_setting.gd
+++ eldenado/UI/Panels/GameSettings/game_setting.gd
@@ -1,8 +1,10 @@
-extends Panel
+extends PanelContainer
 
 onready var resolution_options:OptionButton = find_node('resolution_options')
-onready var chk_fullscreen := $container/m/scroll_container/col_1/left/is_full_screen
-onready var chk_v_sync := $container/m/scroll_container/col_1/left/is_verticle_sync
+onready var chk_fullscreen := $"%is_full_screen"
+onready var chk_v_sync := $"%is_verticle_sync"
+onready var skill_keys := $"%skill_keys"
+var skill_key := preload("res://UI/Panels/GameSettings/skill_key.tscn")
 
 var is_full_screen = false
 var is_verticle_sync = false
@@ -10,6 +12,34 @@ var is_verticle_sync = false
 func _ready() -> void:
 	chk_fullscreen.pressed = Store.settings.fullscreen
 	chk_v_sync.pressed = Store.settings.v_sync
+	load_skill_keys()
+	pass
+
+func load_skill_keys():
+	if not is_instance_valid(Store) and not "settings" in Store:
+		print("Game Setting Class is not in AutoLoad.")
+		return
+	var settings = Store.settings
+	var _kbs = settings.key_bindings
+	for o in _kbs:
+		var item = _kbs[o]
+		if o == "sep":
+			var sep := HSeparator.new()
+			skill_keys.add_child(sep)
+			continue
+		var inst_skill_key = skill_key.instance()
+		inst_skill_key.name = o
+		inst_skill_key.get_node("button").connect("button_up",self,"_on_binding_key",[o])
+		inst_skill_key.get_node("label").text = o
+		if item["key_code"] == -1:
+			inst_skill_key.get_node("key_name").text = "-"
+		skill_keys.add_child(inst_skill_key)
+		print(_kbs[o])
+	pass
+
+func _on_binding_key(node_name):
+	var node := skill_keys.get_node(node_name)
+	print(node_name,node)
 	pass
 
 func close_dialog():
