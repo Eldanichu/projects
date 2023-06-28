ce22acc0c19c4d0bcc25c2ec82a52615f8bd9822
Eldan <76548040@qq.com>
2023-06-28 17:32:28 +0800

    UI base

--- godot c sharp game/Script/Object/Monster/MonsterNode.cs
+++ godot c sharp game/Script/Object/Monster/MonsterNode.cs
@@ -4,134 +4,19 @@ using godotcsharpgame.Script.Util;
 
 namespace godotcsharpgame.Script.Object.Monster {
   public class Monster : Node2D {
-
-    private PathFinding _map;
-    private Vector2[] MovePath;
-    private Tween _tweenMove;
-    private Tick moveTick;
-    private Vector2 lastCell = Vector2.Zero;
-
-    private Polygon2D ViewRange;
-    private Vector2[] ViewRangeShape = new Vector2[4];
-    private Vector2[] ViewRangeShapePositionRelative = new Vector2[4];
-    private Vector2 viewRangeSize;
-
-    private PlayerNode _player;
-
-    [Export] public float moveInterval = 3f;
-    public int TileIndex { set; get; }
-    public Vector2 intersectPoint { get; set; }
-
     public override void _Ready() {
       SetupNode();
     }
 
     private void SetupNode() {
-      _tweenMove = new Tween();
-      AddChild(_tweenMove);
-      moveTick = new Tick(0, moveInterval);
-      AddChild(moveTick);
-      _map = TNode.GetNode<PathFinding>(GetTree(), "%map");
-      var _tc = TNode.GetNode<TileMap>(GetTree(), "tc");
 
-      _player = _tc.GetNode<PlayerNode>("%Player");
 
-      ViewRange = new Polygon2D() {
-        Modulate = Color.Color8(255, 255, 255, 55)
-      };
-      AddChild(ViewRange);
-      Reset();
-      UpdateTileIndex();
-      CreateViewRange(_map.CellSize, new Vector2(1, 1));
-      // moveTick.OnTick += RandMovement;
-      // moveTick.Start();
     }
-
-
-    public void RandMovement(int tick) {
-      var mapPosition = _map.WorldToMap(Position);
-      var nextDir = _map.GetRandomWalkableDir(mapPosition);
-      if (lastCell != Vector2.Zero) {
-        // L.t($"{lastCell}");
-        _map.TC.SetCellv(lastCell, (int)Global.TILE_TYPE.INVALID);
-      }
-
-      var cell = _map.WorldToMap(Position) + nextDir;
-      var point = _map.GetWorldCellVector2(cell);
-      _tweenMove.InterpolateProperty(this,
-              "position",
-              Position,
-              point,
-              0.1f,
-              Tween.TransitionType.Cubic,
-              Tween.EaseType.OutIn
-      );
-      _map.TC.SetCellv(cell, (int)Global.TILE_TYPE.MONSTER);
-      lastCell = cell;
-      _tweenMove.Start();
-      moveTick.Start();
-      UpdateTileIndex();
-      UpdateViewRangePosition();
-
-      // intersectPoint = _map.WorldToMap(PlayerNode.Position);
-    }
-
-    public void CreateViewRange(Vector2 cellSize, Vector2 range) {
-      var cellArea = cellSize.x + cellSize.y;
-      var size = cellArea * 0.25f;
-      var origin = -size;
-      // var rangePoly = _map.GetRange(Position, range);
-      viewRangeSize = cellSize * (range * -1);
-      //up left
-      ViewRangeShape[0] = new Vector2(origin, origin);
-      // up right
-      ViewRangeShape[1] = new Vector2(cellArea * range.x + size, origin);
-      // down right
-      ViewRangeShape[2] = new Vector2(cellArea * range.x + size, cellArea * range.y + size);
-      // down left
-      ViewRangeShape[3] = new Vector2(origin, cellArea * range.y + size);
-      ViewRange.Polygon = ViewRangeShape;
-
-      for (int i = 0; i < ViewRangeShape.Length; i++) {
-        ViewRangeShapePositionRelative[i] = Position - ViewRangeShape[i] + _map.CellSize;
-      }
-
-      UpdateViewRangePosition();
-    }
-
-    private void UpdateViewRangePosition() {
-      var mousePosition = Position;
-      var mapMousePosition = _map.WorldToMap(mousePosition);
-      var halfCellSize = _map.CellSize * 0.5f;
-      var cellSizedPosition = _map.MapToWorld(mapMousePosition) + halfCellSize;
-      ViewRange.Position = cellSizedPosition - Position + viewRangeSize;
-    }
-
+    
     public override void _PhysicsProcess(float delta) {
-      _player.GetPlayerShape();
 
-      var intersect = Geometry.IntersectPolygons2d(
-              _player.PlayerShape,
-              ViewRangeShapePositionRelative
-      );
-      if (intersect.Count <= 0) {
-        return;
-      }
-
-      var poly = (Vector2[])intersect[0];
-      var point = poly[0] - _map.CellSize;
-      var playerPos = _map.WorldToMap(point);
-      L.t($" - {playerPos} - tileType {_map.GetPositionObjectType(point)}");
     }
-
-    private void Reset() {
-      lastCell = _map.WorldToMap(Position);
-      _map.TC.SetCellv(lastCell, (int)Global.TILE_TYPE.MONSTER);
-    }
-
-    private void UpdateTileIndex() {
-      var mapPosition = _map.WorldToMap(Position);
-      TileIndex = _map.GetPointIndex(mapPosition);
-    }
+    
+    
   }
 }
--- godot c sharp game/Script/Object/Player/PlayerNode.cs
+++ godot c sharp game/Script/Object/Player/PlayerNode.cs
@@ -8,51 +8,23 @@ using godotcsharpgame.Script.Util;
 namespace godotcsharpgame.Script.Object.Player {
   public class PlayerNode : Node2D {
     private DamageObject _dmage;
-
-
-    private PathFinding _map;
-    private Tween _tweenMove;
-    private Polygon2D AttackArea;
-    private readonly Vector2[] AttackAreaShape = new Vector2[4];
-
-    private Line2D dirLine;
     private ProgressBar expBar;
     private TextureProgress hpBar;
     private Label hpText;
-    private Vector2 lastCell = Vector2.Zero;
 
     private Vector2[] MovePath;
-    private Tick moveTick;
     private TextureProgress mpBar;
     private Label mpText;
     
-    public int MoveIndex;
-    public float moveInterval = 0.2f;
     public PlayerProperties props;
-    public Vector2[] PlayerShape { set; get; }
     public PlayerObject PlayerObject { set; get; }
 
     public override void _Ready() {
       SetupNode();
-      ResetPlayer();
       _PlayerEvent();
     }
 
     private void SetupNode() {
-      _tweenMove = new Tween();
-      AddChild(_tweenMove);
-      moveTick = new Tick(0, moveInterval);
-      AddChild(moveTick);
-      moveTick.OnTick += MoveAlongPath;
-
-      _map = TNode.GetNode<PathFinding>(GetTree(), "%map");
-      dirLine = GetNode<Line2D>("%lineDir");
-
-      AttackArea = new Polygon2D {
-        Modulate = Color.Color8(255, 255, 255, 55)
-      };
-      AddChild(AttackArea);
-
       var g = GetTree().Root.GetNodeOrNull("main");
       var hp = g.GetNode("%hp");
       var mp = g.GetNode("%mp");
@@ -95,37 +67,6 @@ namespace godotcsharpgame.Script.Object.Player {
       QueueFree();
     }
 
-    private void MoveAlongPath(int count) {
-      if (MovePath == null || MovePath.Length <= 0) return;
-
-      if (MoveIndex >= MovePath.Length) {
-        moveTick.Stop();
-        return;
-      }
-
-      var movePathLength = Math.Min(MoveIndex + 1, MovePath.Length - 1);
-      var nextCell = MovePath[movePathLength];
-      var nextPoint = _map.GetWorldCellVector2(nextCell);
-      if (_map.CellHasObjectTC(nextPoint) || _map.CellHasObject(nextPoint)) return;
-
-      if (lastCell != Vector2.Zero) _map.TC.SetCellv(lastCell, (int)Global.TILE_TYPE.INVALID);
-
-      var cell = MovePath[MoveIndex];
-      var point = _map.GetWorldCellVector2(cell);
-      _tweenMove.InterpolateProperty(this,
-              "position",
-              Position,
-              point,
-              0.1f,
-              Tween.TransitionType.Cubic,
-              Tween.EaseType.OutIn
-      );
-      _map.TC.SetCellv(cell, (int)Global.TILE_TYPE.PLAYER);
-      lastCell = cell;
-      _tweenMove.Start();
-      MoveIndex++;
-    }
-
     private void DoDamage() {
 
     }
@@ -146,62 +87,16 @@ namespace godotcsharpgame.Script.Object.Player {
       if (!(@event is InputEventMouseButton mb)) return;
       if (mb.ButtonIndex != 1 || !mb.Pressed) return;
       var mousePosition = GetGlobalMousePosition();
-      var dist = _map.GetDistance(Position, mousePosition);
-      L.t($"{dist}");
-
-      // CreateViewRange(_map.CellSize, new Vector2(1, 1));
-
-      moveTick.Stop();
-      MovePath = _map.GetMovePath(mousePosition, Position);
-      MoveIndex = 0;
-      moveTick.Start();
+  
     }
 
     private void PlayerMouseMoveEvent(InputEvent @event) {
       if (!(@event is InputEventMouseMotion)) return;
-      var mouseDir = _map.GetAttackDir(Position);
-      var cellSizedDir = mouseDir * _map.CellSize;
-      // if (_map.CellHasObject(Position + cellSizedDir)) {
-      //   return;
-      // }
 
-      CreateAttackArea(_map.CellSize, cellSizedDir);
-      dirLine.Rotation = mouseDir.Angle();
     }
-
-    public void CreateAttackArea(Vector2 cellSize, Vector2 dir) {
-      var cellArea = cellSize.x + cellSize.y;
-      var size = cellArea * 0.25f;
-      var origin = -size;
-      AttackAreaShape[0] = new Vector2(origin, origin);
-      AttackAreaShape[1] = new Vector2(size, origin);
-      AttackAreaShape[2] = new Vector2(size, size);
-      AttackAreaShape[3] = new Vector2(origin, size);
-      AttackArea.Polygon = AttackAreaShape;
-      AttackArea.Position = Position - (Position - dir);
-    }
-
-    public void GetPlayerShape() {
-      var cellSize = _map.CellSize;
-      var cellArea = cellSize.x + cellSize.y;
-      var size = cellArea * 0.25f;
-      var origin = -size;
-      PlayerShape = new Vector2[4];
-      PlayerShape[0] = Position - new Vector2(origin, origin);
-      PlayerShape[1] = Position - new Vector2(size, origin);
-      PlayerShape[2] = Position - new Vector2(size, size);
-      PlayerShape[3] = Position - new Vector2(origin, size);
-    }
-
+    
     public override void _Draw() {
-      if (PlayerShape == null) return;
-
-      DrawColoredPolygon(PlayerShape, Colors.Red);
-    }
-
-    private void ResetPlayer() {
-      lastCell = _map.WorldToMap(Position);
-      _map.TC.SetCellv(lastCell, (int)Global.TILE_TYPE.PLAYER);
+  
     }
 
     private void UIUpdating() {
