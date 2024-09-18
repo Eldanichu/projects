b4b27c5ab13355f60841cc428603440dbffc9cd8
Eldan <76548040@qq.com>
2024-09-18 18:37:44 +0800

    u

--- GMir-4.2/scripts/utils/Node/UNode.gd
+++ GMir-4.2/scripts/utils/Node/UNode.gd
@@ -3,23 +3,20 @@ class_name UNode
 
 var tree_nodes:Dictionary = {}
 
-var _parent:Node
+var _get_class_parent_nodearent:Node
 func _init(parent:Node) -> void:
-	_parent = parent
+	_get_class_parent_nodearent = parent
 
-
-func add_node(id:String, node:Node, parent:Node = null):
-	if id == "" or id == null:
-		return null
-	var _node_name = id
-	if check_if_duped_node_name(id,_p(parent)):
-		_node_name = "{0}_{1}".format([id, hash(id)])
-	node.name = _node_name
-	node.unique_name_in_owner = true
-	_p(parent).add_child(node)
-	var _node = _p(parent).get_node_or_null("{0}".format([_node_name]))
-	tree_nodes[_node_name] = _node
-	return _node
+func add_node(node_name:String, node_obj:Node, parent:Node = null):
+	var _node_name = _get_new_node_name(node_name, parent)
+	node_obj.name = _node_name
+	
+	var _parent_node = _get_class_parent_node(parent)
+	_parent_node.add_child(node_obj)
+	
+	var _node_ref = _parent_node.get_node_or_null(_node_name)
+	tree_nodes[_node_name] = _node_ref
+	return _node_ref
 
 func get_node_ex(node_name:String):
 	if node_name in tree_nodes:
@@ -29,10 +26,10 @@ func get_node_ex(node_name:String):
 func get_nodes() -> Array:
 	return tree_nodes.values()
 
-func remove_node( id:String,parent:Node = null):
-	var _node = get_node_ex(id)
-	_p(parent).remove_child(_node)
-	tree_nodes.erase(id)
+func remove_node(node_name:String,parent:Node = null):
+	var _node = get_node_ex(node_name)
+	_get_class_parent_node(parent).remove_child(_node)
+	tree_nodes.erase(node_name)
 
 func pop_front():
 	var nodes = get_nodes()
@@ -41,8 +38,8 @@ func pop_front():
 	var first = nodes[0]
 	remove_node(first.name)
 
-func check_if_duped_node_name(node_name:String, parent:Node) -> bool:
-	var children:Array = _p(parent).get_children(true)
+func check_if_duped_node_name(node_name:String, parent:Node = null) -> bool:
+	var children:Array = _get_class_parent_node(parent).get_children(true)
 	var is_dupe = false
 	for node in children:
 		if node.name == node_name:
@@ -50,8 +47,25 @@ func check_if_duped_node_name(node_name:String, parent:Node) -> bool:
 	return is_dupe
 
 
-func _p(parent:Node) -> Node:
+func _get_class_parent_node(parent:Node) -> Node:
 	var p = parent
 	if p == null:
-		p = _parent
+		p = _get_class_parent_nodearent
 	return p
+
+func _empty_string(str:String) -> bool:
+	return str == "" or str == null
+
+func _get_hash_name(node_name:String) -> String:
+	return "{0}_{1}".format([node_name, hash(node_name)])
+
+func _get_new_node_name(node_name:String, parent:Node) -> String:
+	var _node_name = node_name
+	if _empty_string(_node_name):
+		_node_name = ""
+	var _parent_node = _get_class_parent_node(parent)
+	if check_if_duped_node_name(_node_name, _parent_node):
+		_node_name = node_name
+	
+	return _node_name
+	
